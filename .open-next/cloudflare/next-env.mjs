e494ca1d1f7663d9ae47e540dc14eaf423e03e87
Justin Toler <jwtoler@gmail.com>
2026-07-17 08:57:28 -0400

    Workflows to work with cloudflare

--- .open-next/cloudflare/next-env.mjs
+++ .open-next/cloudflare/next-env.mjs
@@ -1,3 +1,3 @@
-export const production = {};
-export const development = {};
+export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://ozkkrntbufeugpmeellx.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_secret_8AKsQQEqkzDgyJyjvMaomA_RlkdmE6Y"};
+export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://ozkkrntbufeugpmeellx.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_secret_8AKsQQEqkzDgyJyjvMaomA_RlkdmE6Y"};
 export const test = {};
