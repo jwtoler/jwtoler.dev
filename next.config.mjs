f6458248c3cfeb881434834da8a2f7728f8af5b3
Justin Toler <jwtoler@gmail.com>
2026-07-17 07:06:25 -0400

    Remove Sentry again...

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import { withSentryConfig } from "@sentry/nextjs"
-
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   compress: true,
   productionBrowserSourceMaps: false,
@@ -9,20 +8,4 @@ const nextConfig = {
   trailingSlash: false
 };
 
-const sentryPluginOptions = {
-  automaticVercelMonitors: true,
-  disableLogger: true,
-  hideSourceMaps: true,
-  transpileClientSDK: false,
-  tunnelRoute: "/monitoring",
-  widenClientFileUpload: true,
-};
-
-const sentryWebpackPluginOptions = {
-  org: "justin-toler",
-  project: "jwtoler-dev",
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  silent: true
-};
-
-export default withSentryConfig(nextConfig, sentryWebpackPluginOptions, sentryPluginOptions);
+export default nextConfig;
